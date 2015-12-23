d6b0d5793d06198d854b71a5013bf71c71bf531d
Jack Sun <sunxguo@163.com>
2015-12-23 16:40:38 +0800

    update

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -180,7 +180,7 @@ function getinfo(navid,name){
 					// $("#main_body").html('<a id="link" href="'+result.message.link+'" target="_blank" style="line-height:30px;">'+name+'</a>'+
 					// 	'<script>document.getElementById("link").click();</script>'
 					// );
-					$("#main_body").html('<frame src="'+result.message.link+'" />');
+					$("#main_body").html('<frameset cols="100%"><frame src="'+result.message.link+'" /></frameset>');
 				//	$("#link").click();
 					//alert(result.message.link);
 					//$("#main_body").addClass("con-pd");
