400481656479d29f31f8a0f1cbf0deebe6296bc1
MonkeyKing <MonkeyKing1024@gmail.com>
2016-03-18 19:21:47 +0800

    update

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -194,6 +194,16 @@ function getinfo(navid,name){
 					//alert(result.message.link);
 					//$("#main_body").addClass("con-pd");
 				break;
+				case "7":
+					// window.open(result.message.link);
+					$("#main_body").html('<a id="link" href="tel:'+result.message.link+'" style="line-height:30px;display:none;">'+name+'</a>'+
+						'<script>document.getElementById("link").click();</script>'
+					);
+					// $("#main").html('<div style="width:100%;height:100%;-webkit-overflow-scrolling:touch; overflow: scroll;position:relative;"><div class="url-view-bottom"><button class="url-view-bottom-left" onclick="javascript:bargoback();">Done</button><div class="url-view-bottom-right"><button onclick="javascript:history.go(-1);"><</button><button onclick="javascript:history.go(1);">></button></div></div><iframe src="'+result.message.link+'" frameborder="0" scrolling="yes" style="height: 100%;width:100%;zoom:50%;" /></iframe></div>');
+					//	$("#link").click();
+					//alert(result.message.link);
+					//$("#main_body").addClass("con-pd");
+				break;
 				// case "7":
 				// 	$("#main_body").html('<form id="uploadImgThumb" method="post" action="/cms/index/upload_img" enctype="multipart/form-data">'+
 				// 		'<input onchange="return upload_image(\'#uploadImgThumb\')" name="image" type="file" id="file" style="display:none;" accept="image/*">'+
